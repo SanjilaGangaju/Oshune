aaa8fbc733ff85b533d4a6665067690eb43fe9f8
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-06-30 10:03:12 +0545

    displaying dynamic cards with js

--- src/homeProductCards.js
+++ src/homeProductCards.js
@@ -1,11 +1,19 @@
 const productContainer = document.querySelector('#productContainer');
-const productTemplate = document.querySelector('.productTemplate');
+const productTemplate = document.querySelector('\#productTemplate');
 export const showProductContainer=(products)=>{
     if(!products){
         return false;
     }
     products.forEach((curProd)=>{
         const {brand, category, description, id, image, name, price, stock} = curProd;
-        const product
-    })
+        const productClone = document.importNode(productTemplate.content, true);
+        productClone.querySelector('.category').textContent=category;
+        productClone.querySelector('.productName').textContent=name;
+        productClone.querySelector('.productImage').src = image;
+        productClone.querySelector('.productImage').alt=name;
+        productClone.querySelector('.productStock').textContent= stock;
+        productClone.querySelector('.productDescription').textContent=description;
+
+        productContainer.append(productClone);
+    })  
 }
