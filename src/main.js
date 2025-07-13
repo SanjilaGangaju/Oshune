a618d0c21a1979081dea32eb5e797c2f5124e4e8
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-13 14:55:48 +0545

    adding template for product card

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import  products from "../api/products.json";
+import '../src/style.css';
+
+import { showProductContainer } from "./productContainer";
+console.log(products);
+console.log('hi');
+
+
+showProductContainer(products);
+
---
+++ src/productContainer.js
@@ -0,0 +1,34 @@
+const productContainer = document.querySelectorAll('.product-container');
+console.log(productContainer);
+const productTemplate = document.querySelector('#product-template');
+const bestSelling = document.querySelector('.best-selling--products');
+export const showProductContainer=(products)=>{
+    if (!Array.isArray(products) || products.length==0){
+        console.log('No products to show....');
+        
+    }
+    products.forEach((currProd) => {
+        const {id , name, category, description, price, stock, image} = currProd;
+
+         productContainer.forEach((container)=>{
+                   const productClone= productTemplate.content.cloneNode(true);
+                   productClone.querySelector('.product-name').textContent = name;
+                   productClone.querySelector('.product-card--subheading').textContent= `${stock} remaining in stock`;
+                   productClone.querySelector('.description').textContent= description;
+                   productClone.querySelector('.product-image').src= image;
+                   productClone.querySelector('.product-price').textContent= `₹ ${price}`;
+                   productClone.querySelector('.product-actual-price').textContent= `/ ${price + 30}`;
+
+              container.append(productClone);
+
+        });
+      
+       
+        
+        
+
+    });
+    
+
+
+}
