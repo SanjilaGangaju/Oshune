9062ebc3114909f0063b993616d50d9074f9a2f8
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-18 15:35:06 +0545

    improve responsiveness

--- src/main.js
+++ src/main.js
@@ -17,6 +17,12 @@ productList.addEventListener('click', (e)=>{
     productSection.scrollIntoView({behavior: 'smooth'})
 })
 
+const navMenu = document.querySelector('.nav-menu');
+const menuToggle = document.querySelector('.menu-toggle');
+menuToggle.addEventListener('click',()=>{
+    navMenu.classList.toggle('active');
+    console.log('Classes on navMenu:', navMenu.className);
+})
 showProductContainer(products);
 
 
